ae5809531ba399295a5fd620de6c20618856a32e
Jonas Orrstam <johlson7@gmail.com>
2019-01-07 15:44:15 +0100

    Style fixes for Modal

--- client/src/components/Modal.tsx
+++ client/src/components/Modal.tsx
@@ -6,6 +6,7 @@ import Icon from './Icon';
 
 const Overlay = styled.div`
   position: fixed;
+  z-index: 999;
   left: 0;
   top: 0;
   width: 100vw;
@@ -13,16 +14,17 @@ const Overlay = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  background-color: rgba(0, 0, 0, 0.4);
+  background-color: rgba(252, 252, 252, 0.9);
 `;
 const Header = styled.div`
-  padding-bottom: 5px;
-  border-bottom: 1px solid #ddd;
+  padding-bottom: 10px;
+  border-bottom: 1px dotted #ddd;
   display: flex;
 `;
 
 const Heading = styled.h3`
   flex: 1;
+  color: #333;
 `;
 
 interface IModalProps {
@@ -44,10 +46,10 @@ const Modal: React.StatelessComponent<IModalProps> = ({
 
   return (
     <Overlay>
-      <ModalBox width='300px' bg='background' p="10px">
+      <ModalBox width='500px' bg='rgb(252, 252, 252)' p="30px">
         <Header>
           <Heading>{ heading }</Heading>
-          <Icon onClick={ close } icon={'times'} />
+          <Icon color="#7f7f7f" onClick={ close } icon={'times'} />
         </Header>
         <Box mt="10px">
           { children }
