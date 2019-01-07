b51e1dbfae90addf631d49374f44fceb0af6707c
Jonas Orrstam <johlson7@gmail.com>
2019-01-07 09:18:52 +0100

    Update Box and Flex layout components

--- client/src/components/layout/Box.tsx
+++ client/src/components/layout/Box.tsx
@@ -4,20 +4,28 @@ import {
   width,
   space,
   color,
+  top,
+  boxShadow,
+  position,
   WidthProps,
   SpaceProps,
-  ColorProps
+  ColorProps,
+  BoxShadowProps
 } from 'styled-system';
 
 export type BoxProps = WidthProps
   & SpaceProps
   & ColorProps
+  & BoxShadowProps
   & React.HTMLProps<HTMLDivElement>;
 
 const Box = styled<BoxProps, any>('div')`
   ${width}
   ${space}
   ${color}
+  ${top}
+  ${position}
+  ${boxShadow}
 `;
 
 export default Box
--- client/src/components/layout/Flex.tsx
+++ client/src/components/layout/Flex.tsx
@@ -4,10 +4,15 @@ import {
   flexDirection,
   width,
   height,
+  minHeight,
   FlexDirectionProps,
   justifyContent,
+  justifySelf,
   color,
+  space,
   WidthProps,
+  JustifyContentProps,
+  SpaceProps,
   ColorProps,
   HeightProps
 } from 'styled-system';
@@ -16,14 +21,19 @@ export type FlexProps = & FlexDirectionProps
   & WidthProps
   & ColorProps
   & HeightProps
+  & JustifyContentProps
+  & SpaceProps
   & React.HTMLProps<HTMLDivElement>;
 
 const Flex = styled<FlexProps, any>('div')`
   display: flex;
   ${flexDirection}
   ${justifyContent}
+  ${justifySelf}
+  ${space}
   ${width}
   ${height}
+  ${minHeight}
   ${color}
 `;
 
