7fc5c0330b331dda734aaeaee8d8f63872a9e678
Jonas Orrstam <johlson7@gmail.com>
2019-08-14 13:29:55 +0200

    Add period based comparison stats

--- client/src/components/packages/Strava.tsx
+++ client/src/components/packages/Strava.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react';
 import styled from 'styled-components';
 import CountUp from 'react-countup';
+import Select from 'react-select';
+import * as moment from 'moment';
 import { stravaStore } from '../../stores';
 import api, { getToken } from '../../services/api';
 import { Flex, Box, ProfileWrapper, Button } from '../layout';
-import { useStravaSplits } from '../../hooks/useStravaSplits';
+import { useComparisonStats } from '../../hooks/useComparisonStats';
 import { distanceToKm, secondsTimeObject } from '../../services/helpers';
 
 const Image = styled.img`
@@ -17,13 +19,68 @@ interface IStats {
   moving_time: number;
 }
 
-const SplitsHeader = styled.h3``;
+interface IStatsData {
+  current: number;
+  comparison: number;
+}
+
+interface IPeriodSelect {
+  label: string;
+  value: string;
+}
+
+const StatsTableRow = (props: {
+  title: string;
+  data: IStatsData;
+  decimals: number;
+}) => (
+  <Flex width="100%" m="1px 0" fontSize="20px" color="text">
+    <Flex
+      flex="1"
+      alignItems="center"
+      justifyContent="center"
+      flexDirection="column"
+    >
+      <Box fontSize="13px" color="textLight">
+        {props.title}
+      </Box>
+    </Flex>
+    <Flex
+      flex="1"
+      alignItems="center"
+      flexDirection="column"
+      bg="#fcfcfc"
+      p="7.5px 0"
+      mr="1px"
+    >
+      <CountUp end={props.data.current} decimals={props.decimals} />
+    </Flex>
+    <Flex
+      flex="1"
+      alignItems="center"
+      flexDirection="column"
+      bg="#fcfcfc"
+      p="7.5px 0"
+      ml="1px"
+    >
+      <CountUp end={props.data.comparison} decimals={props.decimals} />
+    </Flex>
+  </Flex>
+);
 
 const Strava: React.FC<{}> = () => {
   const [athlete, setAthlete] = React.useState();
   const [stats, setStats] = React.useState<IStats>();
-  const [splitsSearch, setSplitsSearch] = React.useState<boolean>(false);
-  const splits = useStravaSplits(splitsSearch);
+  const [loadComparisonStats, setLoadComparisonStats] = React.useState<boolean>(
+    false
+  );
+  const [comparisonPeriod, setComparisonPeriod] = React.useState<IPeriodSelect>(
+    { label: 'Week', value: 'week' }
+  );
+  const comparisonStats = useComparisonStats(
+    loadComparisonStats,
+    comparisonPeriod.value as moment.unitOfTime.StartOf
+  );
 
   React.useEffect(() => {
     async function getAthlete() {
@@ -38,7 +95,7 @@ const Strava: React.FC<{}> = () => {
     async function getStats() {
       const stats = await api.get('/packages/strava/athlete/stats', {
         params: {
-          access_token: stravaStore.accessToken,
+          access_token: stravaStore.accessToken
         },
         headers: { Authorization: `Bearer ${getToken()}` }
       });
@@ -46,11 +103,37 @@ const Strava: React.FC<{}> = () => {
     }
 
     getStats();
-  }, [athlete])
+  }, [athlete]);
 
   const showMoreStats = () => {
-    setSplitsSearch(true);
-  }
+    setLoadComparisonStats(true);
+  };
+
+  const options = [
+    { value: 'week', label: 'Week' },
+    { value: 'month', label: 'Month' },
+    { value: 'year', label: 'Year' }
+  ];
+  const styles = {
+    option: (provided: any, state: any) => ({
+      ...provided,
+      fontSize: '13px',
+      cursor: 'pointer'
+    }),
+    container: (base: any) => ({
+      ...base,
+      width: '40%'
+    }),
+    control: (base: any) => ({
+      ...base,
+      border: 'none',
+      fontSize: '13px',
+      cursor: 'pointer'
+    }),
+    indicatorSeparator: () => ({
+      display: 'none'
+    })
+  };
 
   return (
     <Flex
@@ -67,7 +150,13 @@ const Strava: React.FC<{}> = () => {
           </ProfileWrapper>
         )}
       </Box>
-      <Flex width="100%" mt="defaultMargin" fontSize="20px" color="text" mb="10px">
+      <Flex
+        width="100%"
+        mt="defaultMargin"
+        fontSize="20px"
+        color="text"
+        mb="15px"
+      >
         {stats && (
           <>
             <Flex flex="1" alignItems="center" flexDirection="column">
@@ -80,53 +169,105 @@ const Strava: React.FC<{}> = () => {
               <Box fontSize="13px" color="textLight" mb="5px">
                 Distance
               </Box>
-              <CountUp end={distanceToKm(stats.distance)} suffix=" km" decimals={2} />
+              <CountUp
+                end={distanceToKm(stats.distance)}
+                suffix=" km"
+                decimals={2}
+              />
             </Flex>
             <Flex flex="1" alignItems="center" flexDirection="column">
               <Box fontSize="13px" color="textLight" mb="5px">
                 Time
               </Box>
               <Flex flexWrap="wrap">
-                <CountUp end={secondsTimeObject(stats.moving_time).hours} suffix=" h" style={{ marginRight: '5px' }} />
-                <CountUp end={secondsTimeObject(stats.moving_time).minutes} suffix=" min" />
+                <CountUp
+                  end={secondsTimeObject(stats.moving_time).hours}
+                  suffix=" h"
+                  style={{ marginRight: '5px' }}
+                />
+                <CountUp
+                  end={secondsTimeObject(stats.moving_time).minutes}
+                  suffix=" min"
+                />
               </Flex>
             </Flex>
           </>
         )}
       </Flex>
-      { splits && splits.done
-        ?
-          <>
-            <Flex width="100%" mt="10px" fontSize="20px" color="text">
-              <Flex flex="1" alignItems="center" flexDirection="column">
-                <SplitsHeader style={{ fontSize: '16px', color: 'rgba(51,51,51,0.7)' }}>Avarage splits</SplitsHeader>
-              </Flex>
+      {comparisonStats && comparisonStats.done ? (
+        <>
+          <Flex width="100%" m="5px 0" fontSize="20px" color="text">
+            <Flex
+              flex="1"
+              alignItems="center"
+              justifyContent="center"
+              flexDirection="column"
+            >
+              <Select
+                value={comparisonPeriod}
+                options={options}
+                styles={styles}
+                onChange={(e: IPeriodSelect) => {
+                  setComparisonPeriod(e);
+                }}
+              />
             </Flex>
-            <Flex width="100%" m="10px 0" fontSize="20px" color="text">
-              <Flex flex="1" alignItems="center" flexDirection="column">
-                <Box fontSize="13px" color="textLight" mb="5px">
-                  Month
-                </Box>
-                <CountUp end={splits.monthly} decimals={2} />
-              </Flex>
-              <Flex flex="1" alignItems="center" flexDirection="column">
-                <Box fontSize="13px" color="textLight" mb="5px">
-                  Year
-                </Box>
-                <CountUp end={splits.yearly} decimals={2} />
-              </Flex>
-              <Flex flex="1" alignItems="center" flexDirection="column">
-                <Box fontSize="13px" color="textLight" mb="5px">
-                  All time
-                </Box>
-                <Flex flexWrap="wrap">
-                  <CountUp end={splits.all_time} decimals={2} />
-                </Flex>
-              </Flex>
+            <Flex
+              fontSize="13px"
+              justifyContent="center"
+              flex="1"
+              alignItems="center"
+              flexDirection="column"
+              color="textLight"
+            >
+              This {comparisonPeriod.label}
             </Flex>
-          </>
-        : <Button bg="#ffffff" color="rgba(226, 125, 96, 1)" onClick={showMoreStats} >show more</Button>
-      }
+            <Flex
+              fontSize="13px"
+              justifyContent="center"
+              flex="1"
+              alignItems="center"
+              flexDirection="column"
+              color="textLight"
+            >
+              Last {comparisonPeriod.label}
+            </Flex>
+          </Flex>
+
+          <StatsTableRow
+            title="Distance (km)"
+            data={{
+              current: comparisonStats.current.distance,
+              comparison: comparisonStats.comparison.distance
+            }}
+            decimals={2}
+          />
+          <StatsTableRow
+            title="Splits (min/km)"
+            data={{
+              current: comparisonStats.current.splits,
+              comparison: comparisonStats.comparison.splits
+            }}
+            decimals={2}
+          />
+          <StatsTableRow
+            title="Activities"
+            data={{
+              current: comparisonStats.current.activities,
+              comparison: comparisonStats.comparison.activities
+            }}
+            decimals={0}
+          />
+        </>
+      ) : (
+        <Button
+          bg="#ffffff"
+          color="rgba(226, 125, 96, 1)"
+          onClick={showMoreStats}
+        >
+          show more
+        </Button>
+      )}
     </Flex>
   );
 };
---
+++ client/src/hooks/useComparisonStats.ts
@@ -0,0 +1,85 @@
+import * as React from 'react';
+import * as moment from 'moment';
+import { distanceToKm, secondsToMinutes } from '../services/helpers';
+import api, { getToken } from '../services/api';
+import {stravaStore} from '../stores/StravaStore';
+
+interface IStats {
+  current: {
+    distance: number;
+    splits: number;
+    activities: number;
+  },
+  comparison: {
+    distance: number;
+    splits: number;
+    activities: number;
+  }
+  done: boolean;
+}
+
+function requestData(b: moment.Moment, a: moment.Moment) {
+  return {
+    params: {
+      access_token: stravaStore.accessToken,
+      page: 1,
+      before: b.unix(),
+      after: a.unix()
+    },
+    headers: { Authorization: `Bearer ${getToken()}` }
+  }
+}
+
+function mapResponseData(data: any) {
+  let splits: number[] = [];
+  let distance: number = 0;
+
+  data.map((activity: Planner.Strava.Activity) => {
+    const split = parseFloat(secondsToMinutes(activity.moving_time)) / distanceToKm(activity.distance);
+
+    distance = distance + distanceToKm(activity.distance);
+    splits.push(split);
+  });
+
+  const split = splits.reduce((x, y) => x + y, 0) / splits.length;
+
+  return {
+    splits: split,
+    distance: distance,
+    activities: data.length
+  }
+}
+
+export function useComparisonStats(search: boolean, period: moment.unitOfTime.StartOf): IStats | undefined {
+  const [stats, setStats] = React.useState<IStats>();
+
+  const before = moment().endOf(period);
+  const after = moment().endOf(period).subtract(1, period as moment.unitOfTime.DurationConstructor);
+
+  React.useEffect(() => {
+    async function fetchStats() {
+      const response = await api.get('/packages/strava/athlete/activities', requestData(before, after));
+
+      if (response && response.data) {
+        const current = mapResponseData(response.data);
+
+        const comparisonBefore = before.subtract(1, 'year');
+        const comparisonAfter = after.subtract(1, 'year');
+
+        const comparisonResponse = await api.get('/packages/strava/athlete/activities', requestData(comparisonBefore, comparisonAfter));
+
+        if (comparisonResponse && comparisonResponse.data) {
+          const comparison = mapResponseData(comparisonResponse.data);
+
+          setStats( { current: current, comparison: comparison, done: true } );
+        }
+      }
+    }
+
+    if (stravaStore.accessToken && search) {
+      fetchStats();
+    }
+  }, [stravaStore.accessToken, search, period]);
+
+  return stats;
+}
